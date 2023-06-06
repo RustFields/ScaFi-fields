9c68ee2025cb4a66df795c0e98a5ccbe12ebf226
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-06 16:14:41 +0200

    feat: add Defaultable concept

---
+++ src/main/scala/field/Defaultable.scala
@@ -0,0 +1,21 @@
+package field
+
+/** Trait for types that have a default value.
+ * @tparam A
+ *   the Defaultable type
+ */
+trait Defaultable[A]:
+  def default: A
+
+object DefaultableInstances:
+  given Defaultable[Int] with
+    def default = 0
+
+  given Defaultable[String] with
+    def default = ""
+
+  given Defaultable[Boolean] with
+    def default = false
+
+  given [A]: Defaultable[List[A]] with
+    def default: List[A] = Nil
