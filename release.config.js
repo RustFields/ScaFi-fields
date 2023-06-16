4f69129a65a51b0f96f72fec9bafc114448a577a
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-06-16 16:26:50 +0200

    ci: add prepareCommands in Semantic Release configuration

--- release.config.js
+++ release.config.js
@@ -1,17 +1,20 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
-const publishCommands = `
+const prepareCommands = `
 sed -i 's/version := .*/version := "\${nextRelease.version}"/g' build.sbt || exit 1
 git add -A || exit 2
 git commit -m "chore: [skip ci] update version in build.sbt" || exit 3
-git push --force origin || exit 4
-git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md  || exit 5
-git push --force origin \${nextRelease.version} || exit 6
-sbt assembly || exit 7
+git push --force origin || exit 4`
+
+const publishCommands = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md  || exit 1
+git push --force origin \${nextRelease.version} || exit 2
+sbt assembly || exit 3
 `
 const releaseBranches = ["main"]
 config.branches = releaseBranches
 config.plugins.push(
     ["@semantic-release/exec", {
+        "prepareCmd": prepareCommands,
         "publishCmd": publishCommands,
     }],
     ["@semantic-release/github", {
