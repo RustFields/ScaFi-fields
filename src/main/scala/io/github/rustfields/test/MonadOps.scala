df29cfdd89d9c5103b540ca10e7eb211c0880f67
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-25 12:05:39 +0200

    feat: add monad operations utilities

---
+++ src/main/scala/io/github/rustfields/test/MonadOps.scala
@@ -0,0 +1,16 @@
+package io.github.rustfields.test
+
+import cats.Monad
+import cats.implicits.*
+import scala.annotation.targetName
+
+trait MonadOps:
+  extension[F[_] : Monad, A] (fa: F[A])
+    @targetName("mapWith")
+    def >>[B](f: A => B): F[B] =
+      fa.map(f)
+
+    @targetName("flatMapWith")
+    def >>=[B](f: A => F[B]): F[B] =
+      fa.flatMap(f)
+
