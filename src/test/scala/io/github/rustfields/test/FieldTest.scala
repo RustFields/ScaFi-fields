8559bd6ef7611d76b128526f1d8e8cb0b25dc32b
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-25 15:42:39 +0200

    fix: remove unused imports

--- src/test/scala/io/github/rustfields/test/FieldTest.scala
+++ src/test/scala/io/github/rustfields/test/FieldTest.scala
@@ -1,5 +1,6 @@
 package io.github.rustfields.test
 
+import io.github.rustfields.field.{FieldOps, Fields, MonadOps}
 import io.github.rustfields.lang.{FieldCalculusInterpreter, FieldCalculusSyntax}
 
 
