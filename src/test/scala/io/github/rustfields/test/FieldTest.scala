68d5311ed882bc9f6b4962b8658a847c826c29a0
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-28 16:29:10 +0200

    feat: add Field Language Interpreter

--- src/test/scala/io/github/rustfields/test/FieldTest.scala
+++ src/test/scala/io/github/rustfields/test/FieldTest.scala
@@ -1,7 +1,8 @@
 package io.github.rustfields.test
 
+import io.github.rustfields.field.lang.FieldLanguageInterpreter
 import io.github.rustfields.field.{FieldOps, Fields, MonadOps}
 import io.github.rustfields.lang.{FieldCalculusInterpreter, FieldCalculusSyntax}
 
 
-trait FieldTest extends FieldCalculusInterpreter with Fields with FieldOps with MonadOps
+trait FieldTest extends FieldLanguageInterpreter with MonadOps
