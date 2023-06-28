6ba517d5f39384459221d96def5736a8da415b98
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-28 14:23:18 +0200

    feat: implement nbrf and repf

--- src/main/scala/io/github/rustfields/field/lang/FieldLanguage.scala
+++ src/main/scala/io/github/rustfields/field/lang/FieldLanguage.scala
@@ -2,7 +2,7 @@ package io.github.rustfields.field.lang
 
 import io.github.rustfields.field.{FieldOps, Fields}
 import io.github.rustfields.lang.FieldCalculusSyntax
-import io.github.rustfields.vm.Slot.Nbr
+import io.github.rustfields.vm.Slot.{Nbr, Rep}
 
 trait FieldLanguage:
   type F[A]
@@ -19,13 +19,24 @@ trait FieldLanguageImpl extends FieldLanguage with Fields with FieldOps:
     branch(cond)(thn)(els)
 
   override def nbrf[A](expr: => Field[A]): Field[A] =
+    def readNeighbourValue[T](id: Int): Option[T] = vm.context.readExportValue(id, vm.status.path)
+
     vm.nest(Nbr(vm.index))(write = vm.unlessFoldingOnOthers) {
       val nbrs = vm.alignedNeighbours()
-      Field(expr.getMap.filter((nbrId, _) => {
-        nbrs.contains(nbrId)
-      }),
+      Field(
+        nbrs.map(nbrId => { // for each neighbour
+          nbrId -> readNeighbourValue[Field[A]](nbrId) // get the value sent to the current device (`self`)
+            .getOrElse(expr).getMap // otherwise, provide the default
+            .getOrElse(vm.self, // if the neighbour has no value for the current device (`self`)
+              readNeighbourValue[Field[A]](nbrId).getOrElse(expr).default // provide the default
+            )
+        }).toMap,
         expr.default)
     }
 
   override def repf[A](init: => Field[A])(fun: Field[A] => Field[A]): Field[A] =
-    rep(init)(fun)
+    vm.nest(Rep(vm.index))(write = vm.onlyWhenFoldingOnSelf) {
+      // get the old value of the field
+      val oldField = vm.context.readExportValue(vm.self, vm.status.path).getOrElse(init)
+      fun(oldField)
+    }
