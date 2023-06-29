a734280fca6a7a3c0a7e559e81846d84dc8c203c
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-29 15:58:34 +0200

    refactor: rename trait and move to syntax package

--- src/main/scala/io/github/rustfields/field/syntax/MonadSyntax.scala
+++ src/main/scala/io/github/rustfields/field/syntax/MonadSyntax.scala
@@ -1,10 +1,11 @@
-package io.github.rustfields.field
+package io.github.rustfields.field.syntax
 
 import cats.Monad
 import cats.implicits.*
+
 import scala.annotation.targetName
 
-trait MonadOps:
+trait MonadSyntax:
   extension[F[_] : Monad, A] (fa: F[A])
     @targetName("mapWith")
     def >>[B](f: A => B): F[B] =
