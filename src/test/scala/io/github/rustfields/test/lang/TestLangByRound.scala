73ed4acfef5a607a8bd2c23d7db363f91f5d3726
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-29 14:33:39 +0200

    test: completed test suite with the three operators

--- src/test/scala/io/github/rustfields/test/lang/TestLangByRound.scala
+++ src/test/scala/io/github/rustfields/test/lang/TestLangByRound.scala
@@ -7,11 +7,11 @@ import org.scalatest.matchers.should.Matchers
 
 
 class TestLangByRound extends AnyFunSpec with FieldTest with Matchers:
-  val LocalValues, REPF = new ItWord
+  val REPF, NBRF, BRANCHF = new ItWord
 
   val context: Context = ctx(0, Map())
 
-  LocalValues("should be evaluated as Fields") {
+  BRANCHF("should split the computation based on a condition") {
     val lv1 = 1; val lv2 = 2
 
     val res = round(context, branchf(true){lv1}{lv2}).root[Field[Int]]()
@@ -23,4 +23,15 @@ class TestLangByRound extends AnyFunSpec with FieldTest with Matchers:
     val export1 = round(context, program)
     val res = round(ctx(0, Map(mid() -> export1)), program).root[Field[Int]]()
     res shouldBe Field.fromSelfValue(9)
+  }
+
+  NBRF("nbrf") {
+    def program: Field[Int] = nbrf(7)
+    val context0 = ctx(0, Map())
+    val context1 = ctx(1, Map())
+    val export0 = round(context0, program)
+    val export1 = round(context1, program)
+    val context2 = ctx(0, Map(mid() -> export0, 1 -> export1))
+    val res = round(context2, program).root[Field[Int]]()
+    println(res)
   }
