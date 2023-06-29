d29c66a687a24036d72388d57bd65d00d8b49aab
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-29 12:02:08 +0200

    docs: add scaladoc for the functions

--- src/main/scala/io/github/rustfields/field/FieldOps.scala
+++ src/main/scala/io/github/rustfields/field/FieldOps.scala
@@ -9,16 +9,41 @@ trait FieldOps:
 
   export FieldGivens.given
   export cats.instances.all._
-  
+
+  /**
+   * Apply a field of functions to a field
+   * @param f1 the field to apply the function field to
+   * @param f2 the field of functions to apply
+   * @tparam A the type of the argument of the function field
+   * @tparam B the type of the result of the function field
+   * @return a field which is the result of applying the functions from the function field to the values of the first field
+   */
   def applyFunctionField[A, B](f1: Field[A], f2: Field[A => B]): Field[B] =
     for
       v1 <- f1
       v2 <- f2
     yield v2(v1)
 
+  /**
+   * Combines two fields of monoids using the monoid's combine method
+   * @param f1 the first field
+   * @param f2 the second field
+   * @tparam A the type of the fields
+   * @return a field which is the result of combining the values of the two fields using the monoid's combine method
+   */
   def mappendFields[A: Monoid](f1: Field[A], f2: Field[A]): Field[A] =
     combineFields(f1, f2)(_ |+| _)
 
+  /**
+   * Combine two fields using a provided function
+   * @param f1 the first field
+   * @param f2 the second field
+   * @param func the function to combine the values of the fields
+   * @tparam A the type of the first field
+   * @tparam B the type of the second field
+   * @tparam C the type of the result of the function
+   * @return a field which is the result of combining the values of the two fields using the provided function
+   */
   def combineFields[A, B, C](f1: Field[A], f2: Field[B])(
     func: (A, B) => C
   ): Field[C] =
