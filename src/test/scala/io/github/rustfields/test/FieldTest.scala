426419304f3e4b0869d6af0d98b136106eda2fef
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-06-29 13:58:01 +0200

    chore: optimize imports

--- src/test/scala/io/github/rustfields/test/FieldTest.scala
+++ src/test/scala/io/github/rustfields/test/FieldTest.scala
@@ -1,8 +1,4 @@
 package io.github.rustfields.test
 
 import io.github.rustfields.field.lang.FieldLanguageInterpreter
-import io.github.rustfields.field.{FieldOps, Fields, MonadOps}
-import io.github.rustfields.lang.{FieldCalculusInterpreter, FieldCalculusSyntax}
-
-
-trait FieldTest extends FieldLanguageInterpreter with MonadOps
+trait FieldTest extends FieldLanguageInterpreter with TestUtils
