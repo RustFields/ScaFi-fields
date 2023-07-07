6a6a655889c48a4ae9838a58cacec79b3a6223d5
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-07-07 15:07:01 +0200

    feat: add syntax for folding fields

--- src/main/scala/io/github/rustfields/field/syntax/FieldSyntax.scala
+++ src/main/scala/io/github/rustfields/field/syntax/FieldSyntax.scala
@@ -2,10 +2,11 @@ package io.github.rustfields.field.syntax
 
 import io.github.rustfields.field.Fields
 
+import scala.annotation.tailrec
+
 trait FieldSyntax:
   self: Fields =>
   
-  import Field.*
-  
   extension[A](fld: Field[A])
-    def sV: A = selfValue(fld)
+    def selfValue: A = Field.selfValue(fld)
+    def fold(aggr: (A, A) => A): A = Field.fold(fld)(aggr)
