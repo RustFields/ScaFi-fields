e4c3d8558c31aa39c8670ca34d714a753d0a517e
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-07-10 23:08:25 +0200

    test: update test with Field Language

--- src/test/scala/io/github/rustfields/test/lang/TestByEquivalence.scala
+++ src/test/scala/io/github/rustfields/test/lang/TestByEquivalence.scala
@@ -3,14 +3,17 @@ package io.github.rustfields.test.lang
 import io.github.rustfields.test.{FieldTest, TestUtils}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import io.github.rustfields.field.DefaultableInstances.given_Defaultable_Int
+import org.scalatest.flatspec.AnyFlatSpec
 
+import scala.collection.SeqView.Id
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 
-class TestByEquivalence extends AnyFunSpec with FieldTest with Matchers:
-  val checkThat = new ItWord
+class TestByEquivalence extends AnyFlatSpec with FieldTest with Matchers:
+//  val checkThat = new ItWord
 
   given node: TestByEquivalence = this
 
@@ -19,71 +22,97 @@ class TestByEquivalence extends AnyFunSpec with FieldTest with Matchers:
     val execSequence: Seq[Int] = LazyList.continually(random.nextInt(3)).take(100)
     val devicesAndNbrs: Map[Int, List[Int]] = fullyConnectedTopologyMap(mutable.Iterable(0, 1, 2))
 
-  checkThat("multiple nbrs in fold work well"){
+  "fold" should "work with multiple nbrs" in {
     val fixture = new Fixture
+    val f1: () => Field[Int] = () => nbrf(1) + nbrf(2) + nbrf(mid())
+    val f2: () => Field[Int] = () => nbrf(1 + 2 + mid())
 
     assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
-      foldhood(0)(_ + _)(nbr(1) + nbr(2) + nbr(mid()))
+      foldhoodf(f1())(_ + _)
     } {
-      foldhood(0)(_ + _)(nbr(1 + 2 + mid()))
+      foldhoodf(f2())(_ + _)
     }
   }
 
-  checkThat("nbr.nbr is to be ignored") {
+  //todo check this
+  /**
+   * In the original scafi-core the test is:
+   * assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
+   *     foldhood(0)(_ + _)(nbr(mid() + nbr(mid())))
+   *  } {
+   *     2 * foldhood(0)(_ + _)(nbr(mid()))
+   *  }
+   */
+  "nbr.nbr" should "be ignored" in {
     val fixture = new Fixture
+    val f1: () => Field[Int] = () => nbrf(mid() + nbrf(mid()))
+    val f2: () => Field[Int] = () => nbrf(mid())
 
     assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
-      foldhood(0)(_ + _)(nbr(mid() + nbr(mid())))
+      foldhoodf(f1())(_ + _)
     } {
-      2 * foldhood(0)(_ + _)(nbr(mid()))
+//      foldhoodf(2 * f2())(_ + _) //equivalent
+      2 * foldhoodf(f2())(_ + _) //not equivalent but is made as the original in scafi-core (see above)
     }
   }
 
-  checkThat("rep.nbr is to be ignored on first argument") {
+  "rep.nbr" should "be ignored on first argument" in {
     val fixture = new Fixture
+    val f1: () => Field[Int] = () => repf(nbrf(mid()))(old => old)
+    val f2: () => Field[Int] = () => repf(mid())(old => old)
 
     assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence){
-      foldhood(0)(_ + _) {
-        rep(nbr(mid()))(old => old)
+      foldhoodf(f1())(_ + _)
+    }{
+      foldhoodf(f2())(_ + _)
+    }
+  }
+
+  "rep.nbr" should "be ignored overall" in {
+    val fixture = new Fixture
+    val f1: () => Field[Int] = () => repf(nbrf(mid())) { old =>
+      old + nbrf(old) + nbrf(mid())
+    }
+    val f2: () => Field[Int] = () => (1) *
+      repf(mid()) { old =>
+        old * 2 + mid()
       }
+
+    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
+      foldhoodf(f1())(_ + _)
     } {
-      foldhood(0)(_ + _) {
-        rep(mid())(old => old)
-      }
+      foldhoodf(f2())(_ + _)
+    }
+  }
+
+  "fold.init nbr" should "be ignored" in {
+    val fixture = new Fixture
+
+    val f1: () => Field[Int] = () => foldhoodf(nbrf(mid()))(_ + _)
+    val f2: () => Field[Int] = () => foldhoodf(mid())(_ + _)
+
+    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
+      foldhoodf(f1())(_ + _)
+    } {
+      foldhoodf(f2())(_ + _)
     }
   }
 
   //todo does not pass
-  checkThat("rep.nbr is to be ignored overall") {
+  // > not equivalent
+  "fold.fold" should "work" in  {
     val fixture = new Fixture
 
     assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
-      foldhood(0)(_ + _) {
-        rep(nbr(mid())) { old =>
-          old + nbr(old) + nbr(mid())
-        }
-      }
+      foldhoodf(foldhoodf(0.0)(_ + _))(_ + _)
     } {
-      foldhood(0)(_ + _)(1) *
-        rep(mid()) { old =>
-          old * 2 + mid()
-        }
+      Math.pow(foldhoodf(0.0)(_ + _), 2)
     }
   }
 
-  checkThat("fold.fold basically works") {
-    val fixture = new Fixture
-
-    assertEquivalence(fixture.devicesAndNbrs, fixture.execSequence) {
-      foldhood(0)(_ + _) {
-        foldhood(0)(_ + _)(1)
-      }
-    } {
-      Math.pow(foldhood(0)(_ + _)(1), 2)
-    }
-  }
-
-  checkThat("performance does not degrade when nesting foldhoods") {
+  //todo does not pass
+  // > class io.github.rustfields.field.Fields$Field cannot be cast to class java.lang.Double
+  "Performance" should "not degrade when nesting foldhoods" in {
     val execSequence = LazyList.continually(Random.nextInt(100)).take(1000)
     val devicesAndNbrs = fullyConnectedTopologyMap(mutable.ArraySeq.from((0 to 99)))
     val max = 0.000001
@@ -94,26 +123,26 @@ class TestByEquivalence extends AnyFunSpec with FieldTest with Matchers:
       execSequence,
       (x: Double, y: Double) => Math.abs(x - y ) / Math.max(Math.abs(x), Math.abs(y)) < max
     ) {
-      foldhood(0.0)(_ + _) {
-        foldhood(0.0)(_ + _) {
-          foldhood(0.0)(_ + _) {
-            foldhood(0.0)(_ + _) {
-              foldhood(0.0)(_ + _) {
-                foldhood(0.0)(_ + _) {
-                  foldhood(0.0)(_ + _) {
-                    foldhood(0.0)(_ + _) {
-                      foldhood(0.0)(_ + _) {
-                        foldhood(0.0)(_ + _)(1.0)
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+      foldhoodf(
+        foldhoodf(
+          foldhoodf(
+            foldhoodf(
+              foldhoodf(
+                foldhoodf(
+                  foldhoodf(
+                    foldhoodf(
+                      foldhoodf(
+                        foldhoodf(0.0)(_ + _)
+                      )(_ + _)
+                    )(_ + _)
+                  )(_ + _)
+                )(_ + _)
+              )(_ + _)
+            )(_ + _)
+          )(_ + _)
+        )(_ + _)
+      )(_ + _)
     } {
-      Math.pow(foldhood(0.0)(_ + _)(1.0), 10)
+      Math.pow(foldhoodf(0.0)(_ + _), 10)
     }
   }
