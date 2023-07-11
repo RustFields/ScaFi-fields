719eed997481267f4730e0517e95eee19489b5db
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-07-11 15:58:09 +0200

    refactor!: Modify nbr signature and implementation

--- src/main/scala/io/github/rustfields/field/lang/FieldLanguage.scala
+++ src/main/scala/io/github/rustfields/field/lang/FieldLanguage.scala
@@ -8,7 +8,7 @@ import io.github.rustfields.vm.Slot.{FoldHood, Nbr, Rep}
 trait FieldLanguage:
   type F[A]
   def repf[A](init: => F[A])(fun: F[A] => F[A]): F[A]
-  def nbrf[A](expr: => F[A]): F[A]
+  def nbrf[A](expr: => A): F[A]
   def branchf[A](cond: F[Boolean])(thn: => F[A])(els: => F[A]): F[A]
   def foldhoodf[A](init: => F[A])(fun: (A, A) => A): F[A]
 
@@ -20,20 +20,12 @@ trait FieldLanguageImpl extends FieldLanguage with Fields with FieldOps with Fie
   override def branchf[A](cond: Field[Boolean])(thn: => Field[A])(els: => Field[A]): Field[A] =
     branch(cond)(thn)(els)
 
-  override def nbrf[A](expr: => Field[A]): Field[A] =
+  override def nbrf[A](expr: => A): Field[A] =
     def readNeighbourValue[T](id: Int): Option[T] = vm.context.readExportValue(id, vm.status.path)
 
     vm.nest(Nbr(vm.index))(write = vm.onlyWhenFoldingOnSelf) {
       val nbrs = vm.alignedNeighbours()
-      Field(
-        nbrs.map(nbrId => { // for each neighbour
-          nbrId -> readNeighbourValue[Field[A]](nbrId) // get the value sent to the current device (`self`)
-            .getOrElse(expr).getMap // otherwise, provide the default
-            .getOrElse(vm.self, // if the neighbour has no value for the current device (`self`)
-              readNeighbourValue[Field[A]](nbrId).getOrElse(expr).default // provide the default
-            )
-        }).toMap,
-        expr.default)
+      Field(nbrs.map(nbrId => nbrId -> readNeighbourValue(nbrId).getOrElse(expr)).toMap, expr)
     }
 
   override def repf[A](init: => Field[A])(fun: Field[A] => Field[A]): Field[A] =
