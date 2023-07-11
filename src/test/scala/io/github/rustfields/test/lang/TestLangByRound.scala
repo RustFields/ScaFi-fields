05a9b85851e7bd9d7cfcf1748951b58b88f327e2
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-07-11 11:35:41 +0200

    test: improve existing tests in TestLangByRound

--- src/test/scala/io/github/rustfields/test/lang/TestLangByRound.scala
+++ src/test/scala/io/github/rustfields/test/lang/TestLangByRound.scala
@@ -8,31 +8,29 @@ import scala.language.implicitConversions
 
 
 class TestLangByRound extends AnyFunSpec with FieldTest with Matchers:
-  val REPF, NBRF, BRANCHF = new ItWord
+  val REPF, NBRF, BRANCHF, FOOLDHOODF = new ItWord
 
   val context: Context = ctx(0, Map())
 
   BRANCHF("should split the computation based on a condition") {
-    val lv1 = 1; val lv2 = 2
-
-    val res = round(context, branchf(true){lv1}{lv2}).root[Field[Int]]()
-    res shouldBe Field.fromSelfValue(1)
+    val lv1 = 1
+    val lv2 = 2
+    round(context, branchf(true){lv1}{lv2}).root[Field[Int]]() shouldBe Field.fromSelfValue(lv1)
+    round(context, branchf(false){lv1}{lv2}).root[Field[Int]]() shouldBe Field.fromSelfValue(lv2)
   }
 
   REPF("should compute a function between rounds") {
-    def program: Field[Int] = repf(7)(_+1)
-    val export1 = round(context, program)
-    val res = round(ctx(0, Map(mid() -> export1)), program).root[Field[Int]]()
-    res shouldBe Field.fromSelfValue(9)
+    def program: Field[Int] = repf(7)(_ + 1)
+    val exp = round(context, program)
+    round(ctx(0, Map(mid() -> exp)), program).root[Field[Int]]() shouldBe Field.fromSelfValue(9)
   }
 
   NBRF("nbrf") {
-    def program: Field[Int] = nbrf(7)
+    def program: Field[Int] = nbrf(1 + 1)
     val context0 = ctx(0, Map())
     val context1 = ctx(1, Map())
     val export0 = round(context0, program)
     val export1 = round(context1, program)
-    val context2 = ctx(0, Map(mid() -> export0, 1 -> export1))
-    val res = round(context2, program).root[Field[Int]]()
-    println(res)
+    val context2 = ctx(0, Map(0 -> export0, 1 -> export1))
+    round(context2, program).root[Field[Int]]() shouldBe Field(Map(0 -> 2),2)
   }
